b3db4591d1a71a9fc550fa17d26402f58d2e4fe4
Mikkareem <mohdirsath1997@gmail.com>
2023-11-30 11:31:04 +0530

    Adding polymorphic serialization for Poll Response

--- src/main/kotlin/com/techullurgy/pollapp/domain/usecases/GetAssignedPollsForUserUseCase.kt
+++ src/main/kotlin/com/techullurgy/pollapp/domain/usecases/GetAssignedPollsForUserUseCase.kt
@@ -2,9 +2,7 @@ package com.techullurgy.pollapp.domain.usecases
 
 import com.techullurgy.pollapp.data.dao.PollAnswersDao
 import com.techullurgy.pollapp.data.dao.PollsDao
-import com.techullurgy.pollapp.domain.models.AppPoll
 import com.techullurgy.pollapp.network.responses.PollResponse
-import com.techullurgy.pollapp.utils.ServiceResult
 
 class GetAssignedPollsForUserUseCase(
     private val pollsDao: PollsDao,
